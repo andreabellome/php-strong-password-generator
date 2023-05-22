bf820553d280f7f66191f45941e97bf6ed94c2bd
Andrea Bellome <andrea.bellome@gmail.com>
2023-05-22 21:19:22 +0200

    comments

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!-- start: PHP -->
 <?php
 
 /* include the functions here */
@@ -24,7 +25,9 @@ $lunghezzaPassword = intval($lunghezzaPassword);
 $randPass = generateRandomString($lunghezzaPassword, $ripetizioni, $selectedPassOpts);
 
 ?>
+<!-- end: PHP -->
 
+<!-- start: HTML -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -123,10 +126,10 @@ $randPass = generateRandomString($lunghezzaPassword, $ripetizioni, $selectedPass
 
     </div>
 
-
     <!-- bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
 </body>
 </html>
+<!-- end: HTML -->
 
